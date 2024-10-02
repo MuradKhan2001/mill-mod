d388f44389aec15ad8953ab1266364853c4bac92
Murodxon <begzod@MacBook-Air-Murodxon.local>
2024-10-02 15:03:31 +0500

    sudo

--- src/Components/KatalogProduct/National/National.jsx
+++ src/Components/KatalogProduct/National/National.jsx
@@ -8,6 +8,10 @@ import ReactPaginate from "react-paginate";
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
+
+
+
+
 const National = () => {
     const nodeRef = useRef(null);
     const [products, setProducts] = useState([
